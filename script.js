9a4a9ec4d7f14de17c64007105d5f94b9f18aa88
JuniperMar <119713003+JuniperMar@users.noreply.github.com>
2025-01-20 02:33:10 +0000

    Image slider

--- script.js
+++ script.js
@@ -21,3 +21,18 @@ navOpenBtn.addEventListener("click", () => {
 navCloseBtn.addEventListener("click", () => {
   nav.classList.remove("openNav");
 });
+
+// Super fun energy slider
+let next = document.querySelector(".next");
+let prev = document.querySelector(".prev");
+let slider = document.querySelector(".slider");
+
+next.addEventListener('click', function(){
+    let slides = document.querySelectorAll(".slides");
+    slider.appendChild(slides[0]);
+})
+
+prev.addEventListener('click', function(){
+    let slides = document.querySelectorAll(".slides");
+    slider.prepend(slides[slides.length - 1 ]);
+})
